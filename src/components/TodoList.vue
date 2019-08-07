cdcf119b3deba55920c97338215f10daf1a95a67
sergiopetruzzella <sergiopetruzzellaperez@gmail.com>
2019-08-07 14:19:05 -0400

    Lista de To-dos

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,27 @@
 <template>
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
         <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App</h1>
-        
+         
+      
         <todo-card class="mx-auto mt-32 ml-16 w-full max-w-lg absolute left-0">
             <todo-form></todo-form>
         </todo-card>
 
-        <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg text-gray-700 absolute right-0 mt-32 mr-16">
-            Todo List
-        </todo-card>
+       
+
+         <todo-task v-for="task in tasks" class="mx-auto mt-4 h-64 w-full max-w-lg text-gray-700 absolute right-0 mt-32 mr-16" > 
+            <div class="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+              <div class=" relative sm:flex sm:items-center px-0 py-4">
+                <div class="mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left">
+                  <p class="text-xl leading-tight">{{task.title}}</p>
+                  <p class="pt-1 text-sm leading-tight text-gray-600">Customer Support Specialst</p>
+                </div>
+             <button class=" mr-2 absolute right-0 text-purple-500 hover:text-white hover:bg-purple-500 border border-purple-500 text-xs font-semibold rounded-full px-4 py-1 leading-normal">Message</button>
+            
+              </div>
+            </div>
+         </todo-task>
+
 
 <!--        <todo-card class="mx-auto mt-10 w-full max-w-lg text-gray-700">
             Create/Edit Task
@@ -29,7 +42,14 @@ export default {
                 {
                     id: 1,
                     title: 'Example task',
-                    done: false
+                    done: false,
+                    description: 'Es necesario qeu vaya a ese sitio temprano'
+                },
+                {
+                    id: 2,
+                    title: 'Example task2',
+                    done: false,
+                    description: '',
                 }
             ]
         };
