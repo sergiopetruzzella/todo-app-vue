1afbf631189b8e96063052fc442c92f2f6bc1c0e
Andres Ampies <ampies@typeiqs.com>
2019-08-07 11:52:58 -0400

    finished form structure

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,17 +2,18 @@
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
         <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App</h1>
 
-        <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg">
-            Todo List
-        </todo-card>
-
-        <todo-card class="mx-auto mt-10 w-full max-w-lg">
-            Create/Edit Task
-        </todo-card>
-
         <todo-card class="mx-auto mt-10 w-full max-w-lg">
             <todo-form></todo-form>
         </todo-card>
+
+        <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg text-gray-700">
+            Todo List
+        </todo-card>
+
+        <todo-card class="mx-auto mt-10 w-full max-w-lg text-gray-700">
+            Create/Edit Task
+        </todo-card>
+
     </div>
 </template>
 
